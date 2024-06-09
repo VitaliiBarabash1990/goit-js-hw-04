58570a583a286569ae83fd345dcfc5937ca245c3
Vitalii <alievruslan1818@gmail.com>
2024-06-09 13:50:46 +0200

    1

---
+++ js/task-3.js
@@ -0,0 +1,26 @@
+console.log("task-3");
+
+const profile = {
+	username: "Jacob",
+	playTime: 300,
+	changeUsername(newName) {
+		this.username = newName;
+		return this.username;
+	},
+	updatePlayTime(hours) {
+		this.playTime += hours;
+		return profile.playTime;
+	},
+	getInfo() {
+		return `${this.username} has ${this.playTime} active hours!`
+	},
+};
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
